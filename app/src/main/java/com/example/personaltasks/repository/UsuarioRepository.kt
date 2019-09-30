955c5571b2ade5371cd8747816c29f3f5d3dd164
Danilo Valerio Silva <dvalerio@everis.com>
2019-09-30 14:54:58 -0300

    Inserção de DADOS

--- app/src/main/java/com/example/personaltasks/repository/UsuarioRepository.kt
+++ app/src/main/java/com/example/personaltasks/repository/UsuarioRepository.kt
@@ -1,6 +1,8 @@
 package com.example.personaltasks.repository
 
+import android.content.ContentValues
 import android.content.Context
+import com.example.personaltasks.constants.DataBaseConstants
 
 /**
  * Responsável pela inserção da classe usuário
@@ -23,5 +25,18 @@ class UsuarioRepository private constructor(context: Context){
         }
         //variável que armazena a instância da classe
         private var INSTANCE: UsuarioRepository? = null
+    }
+
+    //faz a inserção e retorna o último ID inserido
+    fun insert(nome: String, email: String, senha: String): Int{
+        //ações no banco readableDatabase: SELECT, writableDatabase: INSERT, DELETE, UPDATE
+        val db = mPersonalTasksDataBaseHelper.writableDatabase //abre conexão para escrita
+
+        val insertValues = ContentValues()
+        insertValues.put(DataBaseConstants.USUARIO.COLUMNS.NOME, nome)
+        insertValues.put(DataBaseConstants.USUARIO.COLUMNS.EMAIL, email)
+        insertValues.put(DataBaseConstants.USUARIO.COLUMNS.SENHA, senha)
+
+        return db.insert(DataBaseConstants.USUARIO.TABLE_NAME, null, insertValues).toInt()
     }
 }
