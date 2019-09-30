bc0036df9ac1a56e8535b11e18a0c2ab0ec226e1
Danilo Valerio Silva <dvalerio@everis.com>
2019-09-30 11:36:37 -0300

    Register Activity - Layout e eventos

--- app/src/main/java/com/example/personaltasks/RegisterActivity.kt
+++ app/src/main/java/com/example/personaltasks/RegisterActivity.kt
@@ -2,11 +2,36 @@ package com.example.personaltasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_register.*
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        setListeners()
+    }
+
+    override fun onClick(view: View) {
+
+        when(view.id){
+            R.id.btnSalvar -> {
+                Toast.makeText(this, "BTN SALVAR", Toast.LENGTH_SHORT).show()
+                salvarUsuario()
+            }
+        }
+
+    }
+
+    private fun setListeners(){
+        btnSalvar.setOnClickListener(this)
+
+    }
+
+    private fun salvarUsuario(){
+        
     }
 }
