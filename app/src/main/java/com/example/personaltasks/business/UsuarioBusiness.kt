78916043e4c404196ff8224f82973359de8c4b0c
Danilo Valerio Silva <dvalerio@everis.com>
2019-09-30 15:24:52 -0300

    ValidationException Util

--- app/src/main/java/com/example/personaltasks/business/UsuarioBusiness.kt
+++ app/src/main/java/com/example/personaltasks/business/UsuarioBusiness.kt
@@ -2,6 +2,8 @@ package com.example.personaltasks.business
 
 import android.content.Context
 import com.example.personaltasks.repository.UsuarioRepository
+import com.example.personaltasks.util.ValidationException
+import java.lang.Exception
 
 /**
  * Essa classe é responsável por tratar toda a lógica de negócio do usuário
@@ -12,7 +14,16 @@ class UsuarioBusiness (val context: Context) {
     val mUsuarioRepository : UsuarioRepository = UsuarioRepository.getInstance(context)
 
     fun insert(nome: String, email: String, senha: String){
-        val userId = mUsuarioRepository.insert(nome, email, senha)
-        val  str = ""
+
+       try {
+           if(nome.isNullOrEmpty() || email.isNullOrEmpty() || senha.isNullOrEmpty() ){
+               throw ValidationException("Informe os campos!")
+           }
+
+           val userId = mUsuarioRepository.insert(nome, email, senha)
+
+       } catch (e: Exception){
+           throw e
+       }
     }
 }
---
+++ app/src/main/java/com/example/personaltasks/util/ValidationException.kt
@@ -0,0 +1,3 @@
+package com.example.personaltasks.util
+
+class ValidationException(message: String) : Exception(message)
