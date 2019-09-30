f91f796da3279d0777dcdac9e5c097190fe0bc70
Danilo Valerio Silva <dvalerio@everis.com>
2019-09-30 14:46:10 -0300

    Passagem de Context para acesso ao banco

--- app/src/main/java/com/example/personaltasks/repository/UsuarioRepository.kt
+++ app/src/main/java/com/example/personaltasks/repository/UsuarioRepository.kt
@@ -1,5 +1,7 @@
 package com.example.personaltasks.repository
 
+import android.content.Context
+
 /**
  * Responsável pela inserção da classe usuário
  * Para evitar concorrência todas as classes repository deve ser Singleton
@@ -8,12 +10,14 @@ package com.example.personaltasks.repository
  *  - Tenha um método que retorna a instância da classe (getInstance)
  */
 
-class UsuarioRepository private constructor(){
+class UsuarioRepository private constructor(context: Context){
+    //para acessar o banco
+    private var mPersonalTasksDataBaseHelper : PersonalTasksDataBaseHelper = PersonalTasksDataBaseHelper(context)
 
     companion object{
-        fun getInstance() : UsuarioRepository {
+        fun getInstance(context: Context) : UsuarioRepository {
             if(INSTANCE == null){
-                INSTANCE = UsuarioRepository()
+                INSTANCE = UsuarioRepository(context)
             }
             return INSTANCE as UsuarioRepository
         }
