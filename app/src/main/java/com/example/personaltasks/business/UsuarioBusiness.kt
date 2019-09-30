72f71cf4d7dfea3141059c6891706b8d03fe25a7
Danilo Valerio Silva <dvalerio@everis.com>
2019-09-30 15:04:37 -0300

    UsuárioBusiness criada

---
+++ app/src/main/java/com/example/personaltasks/business/UsuarioBusiness.kt
@@ -0,0 +1,17 @@
+package com.example.personaltasks.business
+
+import android.content.Context
+import com.example.personaltasks.repository.UsuarioRepository
+
+/**
+ * Essa classe é responsável por tratar toda a lógica de negócio do usuário
+ */
+
+class UsuarioBusiness (val context: Context) {
+    //instancia a usuario repository e executa a função insert
+    val mUsuarioRepository : UsuarioRepository = UsuarioRepository.getInstance(context)
+
+    fun insert(nome: String, email: String, senha: String){
+        val userId = mUsuarioRepository.insert(nome, email, senha)
+    }
+}
