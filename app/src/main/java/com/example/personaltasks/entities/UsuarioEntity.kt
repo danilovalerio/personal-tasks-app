0e7d79fc95cab2c91341144c4080a37fc335f3f0
Danilo Valerio Silva <dvalerio@everis.com>
2019-10-01 10:32:48 -0300

    Usuario Entity para transitar entre camadas

---
+++ app/src/main/java/com/example/personaltasks/entities/UsuarioEntity.kt
@@ -0,0 +1,6 @@
+package com.example.personaltasks.entities
+
+/**
+ * Classe somente de dados que servirá para transitar entre as camadas
+ */
+data class UsuarioEntity (val id: Int, var nome: String, var email: String, var senha: String)
